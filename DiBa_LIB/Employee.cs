1cea98f0a2c0e8399bf5771b9545704db19d2e4a
andrebayup14 <abayup1498@gmail.com>
2022-11-29 20:25:21 +0700

    Update Selasa

--- DiBa_LIB/Employee.cs
+++ DiBa_LIB/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,93 @@ namespace DiBa_LIB
 {
     public class Employee
     {
+        private int id;
+        private string nama_depan;
+        private string nama_keluarga;
+        private Position position;
+        private string nik;
+        private string email;
+        private string password;
+        private DateTime tgl_buat;
+        private DateTime tgl_perubahan;
+
+        public Employee(int id, string nama_depan, string nama_keluarga, Position position, string nik, string email, string password, 
+                        DateTime tgl_buat, DateTime tgl_perubahan)
+        {
+            this.Id = id;
+            this.Nama_depan = nama_depan;
+            this.Nama_keluarga = nama_keluarga;
+            this.Position = position;
+            this.Nik = nik;
+            this.Email = email;
+            this.Password = password;
+            this.Tgl_buat = tgl_buat;
+            this.Tgl_perubahan = tgl_perubahan;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nama_depan { get => nama_depan; set => nama_depan = value; }
+        public string Nama_keluarga { get => nama_keluarga; set => nama_keluarga = value; }
+        public Position Position { get => position; set => position = value; }
+        public string Nik { get => nik; set => nik = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public DateTime Tgl_buat { get => tgl_buat; set => tgl_buat = value; }
+        public DateTime Tgl_perubahan { get => tgl_perubahan; set => tgl_perubahan = value; }
+
+        public static List<Employee> BacaData(string kriteria, string nilaiKriteria)
+        {
+            string sql = "";
+
+            if (kriteria == "")
+            {
+                sql = "SELECT e.id, e.nama_depan, e.nama_keluarga, e.position, e.nik, e.email, e.password, e.tgl_buat, e.tgl_perubahan " +
+                      "from employee e INNER JOIN position p ON e.position = p.id";
+            }
+            else
+            {
+                sql = "SELECT e.id, e.nama_depan, e.nama_keluarga, e.position, e.nik, e.email, e.password, e.tgl_buat, e.tgl_perubahan " +
+                      "FROM employee e INNER JOIN position p ON e.position = p.id " +
+                      "WHERE " + kriteria + " LIKE '%" + nilaiKriteria + "%'";
+            }
+
+            MySqlDataReader hasil = Koneksi.JalankanPerintahQuery(sql);
+
+            List<Employee> listEmployee = new List<Employee>();
+
+            while (hasil.Read() == true)
+            {
+                Position p = new Position(int.Parse(hasil.GetString(3)), hasil.GetString(4), hasil.GetString(5));
+
+                Employee e = new Employee(int.Parse(hasil.GetString(0)), hasil.GetString(1), hasil.GetString(2), p, hasil.GetString(6),
+                                          hasil.GetString(7), hasil.GetString(8), DateTime.Parse(hasil.GetString(9)), DateTime.Parse(hasil.GetString(10)));
+
+                listEmployee.Add(e);
+            }
+
+            return listEmployee;
+        }
+        public static void TambahData(Employee e)
+        {
+            string sql = "INSERT into employee (id, nama_depan, nama_keluarga, position, nik, email, password, tgl_buat, tgl_perubahan) " +
+                         "values (" + e.Id + ", '" + e.Nama_depan + "', '" + e.Nama_keluarga + "', " + e.Position.PositionID + ", '" + e.Nik +
+                         "', '" + e.Email + "', '" + e.Password + "', '" + e.Tgl_buat + "', '" + e.Tgl_perubahan + "')";
+
+            Koneksi.JalankanPerintahDML(sql);
+        }
+        public static void UbahData(Employee e)
+        {
+            string sql = "UPDATE employee set nama_depan = '" + e.Nama_depan + "', nama_keluarga = '" + e.Nama_keluarga + "', position = " +
+                         e.Position.PositionID + ", email = '" + e.Email + "', password = '" + e.Password + "', tgl_buat = '" + e.Tgl_buat + "', " +
+                         "tgl_perubahan = '" + e.Tgl_perubahan + "' where id = " + e.Id;
+
+            Koneksi.JalankanPerintahDML(sql);
+        }
+        public static void HapusData(Employee e)
+        {
+            string sql = "DELETE from employee where id = " + e.id;
+
+            Koneksi.JalankanPerintahDML(sql);
+        }
     }
 }
---
+++ ProjectDatabase_Ivano/FormDaftarEmployee.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ProjectDatabase_Ivano
+{
+    partial class FormDaftarEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewEmployee = new System.Windows.Forms.DataGridView();
+            this.textBoxKriteria = new System.Windows.Forms.TextBox();
+            this.comboBoxKriteria = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonKeluar = new System.Windows.Forms.Button();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployee)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewEmployee
+            // 
+            this.dataGridViewEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmployee.Location = new System.Drawing.Point(14, 147);
+            this.dataGridViewEmployee.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewEmployee.Name = "dataGridViewEmployee";
+            this.dataGridViewEmployee.RowHeadersWidth = 51;
+            this.dataGridViewEmployee.Size = new System.Drawing.Size(803, 287);
+            this.dataGridViewEmployee.TabIndex = 36;
+            // 
+            // textBoxKriteria
+            // 
+            this.textBoxKriteria.Location = new System.Drawing.Point(388, 28);
+            this.textBoxKriteria.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxKriteria.Name = "textBoxKriteria";
+            this.textBoxKriteria.Size = new System.Drawing.Size(369, 22);
+            this.textBoxKriteria.TabIndex = 3;
+            // 
+            // comboBoxKriteria
+            // 
+            this.comboBoxKriteria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxKriteria.FormattingEnabled = true;
+            this.comboBoxKriteria.Items.AddRange(new object[] {
+            "Id Jenis Transaksi",
+            "Kode Transaksi",
+            "Nama Transaksi"});
+            this.comboBoxKriteria.Location = new System.Drawing.Point(175, 27);
+            this.comboBoxKriteria.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxKriteria.Name = "comboBoxKriteria";
+            this.comboBoxKriteria.Size = new System.Drawing.Size(204, 24);
+            this.comboBoxKriteria.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 31);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cari Berdasarkan:";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(803, 39);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "DAFTAR EMPLOYEE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonKeluar
+            // 
+            this.buttonKeluar.BackColor = System.Drawing.Color.Navy;
+            this.buttonKeluar.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKeluar.ForeColor = System.Drawing.Color.White;
+            this.buttonKeluar.Location = new System.Drawing.Point(616, 455);
+            this.buttonKeluar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonKeluar.Name = "buttonKeluar";
+            this.buttonKeluar.Size = new System.Drawing.Size(201, 53);
+            this.buttonKeluar.TabIndex = 38;
+            this.buttonKeluar.Text = "&KELUAR";
+            this.buttonKeluar.UseVisualStyleBackColor = false;
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.BackColor = System.Drawing.Color.Navy;
+            this.buttonTambah.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTambah.ForeColor = System.Drawing.Color.White;
+            this.buttonTambah.Location = new System.Drawing.Point(14, 455);
+            this.buttonTambah.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(201, 53);
+            this.buttonTambah.TabIndex = 37;
+            this.buttonTambah.Text = "&TAMBAH";
+            this.buttonTambah.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lavender;
+            this.panel1.Controls.Add(this.textBoxKriteria);
+            this.panel1.Controls.Add(this.comboBoxKriteria);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(14, 58);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(803, 81);
+            this.panel1.TabIndex = 35;
+            // 
+            // FormDaftarEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 522);
+            this.Controls.Add(this.dataGridViewEmployee);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonKeluar);
+            this.Controls.Add(this.buttonTambah);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormDaftarEmployee";
+            this.Text = "Daftar Employee";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployee)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView dataGridViewEmployee;
+        private System.Windows.Forms.TextBox textBoxKriteria;
+        private System.Windows.Forms.ComboBox comboBoxKriteria;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonKeluar;
+        private System.Windows.Forms.Button buttonTambah;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
